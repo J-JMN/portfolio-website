430c11ffd1a5bd4842b7775aaa1f92b9c1950a1d
Joseph <j.mburu.pro@gmail.com>
2025-05-20 23:13:15 +0300

    re-build

---
+++ my-portfolio/src/components/Header.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './Header.css';
+import profilePic from '../images/profile picture.jpg';
+
+const Header = () => {
+  return (
+    <header className="header">
+      <img src={profilePic} alt="Profile" className="profile-pic" />
+      <h1>Joseph Mburu</h1>
+      <p>Frontend Developer | Designer | Creator</p>
+    </header>
+  );
+};
+
+export default Header;
+
+
