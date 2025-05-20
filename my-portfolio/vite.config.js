ff87fb89c191ee7413273224da26b3960ae1f983
Joseph <j.mburu.pro@gmail.com>
2025-05-21 00:18:37 +0300

    deploy 3

--- my-portfolio/vite.config.js
+++ my-portfolio/vite.config.js
@@ -1,8 +1,10 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/portfolio-website/',
   plugins: [react()],
-});
+  base: '/portfolio-website/', 
+})
+
 
