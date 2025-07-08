704fc4359ef8e3e0b03921a4be746f43ab53613d
Joseph <j.mburu.pro@gmail.com>
2025-07-08 10:44:52 +0300

    formatting updated

--- my-portfolio/src/components/Projects.jsx
+++ my-portfolio/src/components/Projects.jsx
@@ -1,6 +1,6 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './Projects.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Projects.css";
 import project1Img from "../images/Project 1.jpg";
 import project2Img from "../images/Project 2.jpg";
 import project3Img from "../images/Project 3.jpg";
@@ -11,26 +11,37 @@ export const projectsData = [
     id: "tech-time-capsule",
     title: "Tech Time Capsule",
     img: project1Img,
-    description: "A Tech history website that allows users to document milestones related to tech over the years.",
-    detailedDescription: "The Tech Time Capsule is a full-stack web application that serves as a community-driven museum of technological history. Authenticated users can contribute by submitting new events, creating custom categories, and managing their contributions. The platform provides a dynamic and filterable timeline to explore the pivotal moments of tech history.",
-    technologies: ["React", "Flask", "Python", "PostgreSQL", "SQLAlchemy", "RESTful API"],
+    description:
+      "A Tech history website that allows users to document milestones related to tech over the years.",
+    detailedDescription:
+      "The Tech Time Capsule is a full-stack web application that serves as a community-driven museum of technological history. Authenticated users can contribute by submitting new events, creating custom categories, and managing their contributions. The platform provides a dynamic and filterable timeline to explore the pivotal moments of tech history.",
+    technologies: [
+      "React",
+      "Flask",
+      "Python",
+      "PostgreSQL",
+      "SQLAlchemy",
+      "RESTful API",
+    ],
     url: "https://tech-time-capsule-client.onrender.com/",
-    githubUsername: "J-JMN"
+    githubUsername: "J-JMN",
   },
   {
     id: "project-2",
     title: "Project 2",
     img: project2Img,
-    description: "Real-time chat application leveraging Socket.io and React Hooks.",
+    description:
+      "Real-time chat application leveraging Socket.io and React Hooks.",
     // No detailed description to show the "coming soon" message
   },
   {
     id: "project-3",
     title: "Project 3",
     img: project3Img,
-    description: "Customizable dashboard with drag-and-drop widgets and API integrations.",
+    description:
+      "Customizable dashboard with drag-and-drop widgets and API integrations.",
     // No detailed description to show the "coming soon" message
-  }
+  },
 ];
 
 const Projects = () => {
@@ -64,7 +75,3 @@ const Projects = () => {
 };
 
 export default Projects;
-
-
-
-
